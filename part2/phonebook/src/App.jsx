abd66f4e8bdf44f5781b907a3fb15293ef5d23a6
Prabuddha Bhattacharjee <122960910+bhattacharjeeprabuddha@users.noreply.github.com>
2024-09-18 17:20:06 +0530

    solution till 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,8 +3,12 @@ import Numbers from './components/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]); 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+ 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
 
@@ -27,21 +31,32 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       
     } else {
-      const newPerson = { name: newName, number: newNumber };
+      const newPerson = { name: newName, number: newNumber, id: persons.length+1 };
       setPersons(persons.concat(newPerson));
       setNewName('');
       setNewNumber('');
     }
   }
 
+  // event handler for filter by name input
+  const filterByName = (event) => {
+    const name = event.target.value.toLocaleLowerCase();
+    const filteredPersons = persons.filter(p => 
+      p.name.toLocaleLowerCase().includes(name));
+    
+    setPersons(filteredPersons);
 
+  }
 
   return (
     <div>
-      {/* <div>debug: {newName}</div> */}
-
       <h2>Phonebook</h2>
       
+      <div>
+        filter shown with <input type="text" onChange={filterByName}/>
+
+      </div>
+
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
