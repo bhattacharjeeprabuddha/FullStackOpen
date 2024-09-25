ae8d4566fc1b4f7a09fc82120cab501324df4e11
Prabuddha Bhattacharjee <122960910+bhattacharjeeprabuddha@users.noreply.github.com>
2024-09-25 12:32:09 +0530

    resync notes-backend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 
-const cors = require('cors')
-app.use(cors)
-
 let notes = [
   {
     id: 1,
@@ -22,6 +19,8 @@ let notes = [
   }
 ]
 
+app.use(express.static('dist'))
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -29,6 +28,10 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 app.use(requestLogger)
