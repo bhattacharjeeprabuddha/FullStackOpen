222468aaf5308ee4c056ec50cb06194a69a05595
bhattacharjee-prabuddha <bhattacharjeeprabuddha@gmail.com>
2025-04-19 12:24:51 +0000

    solution 3.15

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -36,21 +36,34 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    const person = persons.find(p => p.id===id)
+    Person.findById(request.params.id)
+        .then(p => {
+            if(p) {
+                response.json(p);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => {
+            // console.log(error);
+            // response.status(400).send({error: 'malformatted id'});
+            next(error);
+        }
+    );
 
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter(p => p.id !== id);
-    response.status(204).end();
+    // const id = request.params.id;
+    // persons = persons.filter(p => p.id !== id);
+    // response.status(204).end();
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 })
+
 
 app.post('/api/persons', (request, response) => {
     const name = request.body.name;
