4e3175f205d5d61cfe5643a3d4b1ae986acfbccc
bhattacharjee-prabuddha <bhattacharjeeprabuddha@gmail.com>
2025-07-29 03:52:55 +0000

    part4 solution: 4.6, 4.7

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -5,7 +5,7 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     const likesArray = blogs.map(b => b.likes);
     let total = 0;
-    for (l of likesArray) {
+    for (const l of likesArray) {
         total += l;
     }
     return total;
@@ -15,7 +15,7 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
     let highestLike = 0;
     let favBlog;
-    for (blog of blogs) {
+    for (const blog of blogs) {
         if (blog.likes > highestLike) {
             highestLike = blog.likes;
             favBlog = blog;
@@ -24,11 +24,65 @@ const favoriteBlog = (blogs) => {
     return favBlog;
 }
 
+const mostBlogs = (blogs) => {
+    let authorBlogsCountMap = new Map();
+    for (const blog of blogs) {
+        let author = blog.author;
+        if (authorBlogsCountMap.has(author)) {
+            let blogsCount = authorBlogsCountMap.get(author);
+            authorBlogsCountMap.set(author, blogsCount + 1);
+        } else {
+            authorBlogsCountMap.set(author, 1);
+        }
+    }
+
+    let highestBlogsCount = 0;
+    let authorWithHighestBlogs;
+    for (const [author, blogsCount] of authorBlogsCountMap.entries()) {
+        if (blogsCount > highestBlogsCount) {
+            highestBlogsCount = blogsCount;
+            authorWithHighestBlogs = author;
+        }
+    }
+
+    return { author: authorWithHighestBlogs, blogs: highestBlogsCount };
+}
+
+
+const mostLikes = (blogs) => {
+    let authorLikesCountMap = new Map();
+    for (const blog of blogs) {
+        let author = blog.author;
+        let currentLikes = blog.likes;
+        if (authorLikesCountMap.has(author)) {
+            let previousLikes = authorLikesCountMap.get(author);
+            authorLikesCountMap.set(author, previousLikes + currentLikes);
+        } else {
+            authorLikesCountMap.set(author, currentLikes);
+        }
+    }
+
+    console.log(authorLikesCountMap);
+
+    let highestLikesCount = 0;
+    let authorWithHighestLikes;
+    for (const [author, likesCount] of authorLikesCountMap.entries()) {
+        if (likesCount > highestLikesCount) {
+            highestLikesCount = likesCount;
+            authorWithHighestLikes = author;
+        }
+    }
+
+    return { author: authorWithHighestLikes, likes: highestLikesCount };
+}
+
 
 
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
