7b3b55bd89acf08f804f3a664fa579250453fd5b
bhattacharjee-prabuddha <bhattacharjeeprabuddha@gmail.com>
2025-08-10 07:00:21 +0000

    part4 solution 4.9

--- part4/blogList/tests/blogListApi.test.js
+++ part4/blogList/tests/blogListApi.test.js
@@ -5,6 +5,7 @@ const superTest = require('supertest');
 const app = require('../app');
 const Blog = require('../model/blog');
 const { blogsTestData } = require('../utils/list_helper');
+const blog = require('../model/blog');
 
 const api = superTest(app);
 
@@ -12,7 +13,6 @@ const api = superTest(app);
 
 // seed db before each test
 beforeEach(async () => {
-
     // clean db
     await Blog.deleteMany({});
 
@@ -32,6 +32,18 @@ describe('BlogList App API test', () => {
         const content = response.body;
         assert.strictEqual(content.length, blogsTestData.length);
 
+    });
+
+    test('The unique identifier property of the blog posts is "id"', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200);
+
+        const blogIds = response.body.map(b => b.id);
+        assert.strictEqual(blogIds.length, blogsTestData.length);
+        for (const blog of response.body) {
+            assert(!Object.keys(blog).includes("_id"));
+        }
     });
 });
 
