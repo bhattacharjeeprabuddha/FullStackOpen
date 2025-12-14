8c9209337048b4f4ec2b76cf4c62f40524027f10
bhattacharjee-prabuddha <bhattacharjeeprabuddha@gmail.com>
2025-12-14 07:58:25 +0000

    part4/blogList/middleware/userExtractor.js: add jwt error handling

--- part4/blogList/middleware/userExtractor.js
+++ part4/blogList/middleware/userExtractor.js
@@ -7,12 +7,22 @@ const userExtractor = async (req, res, next) => {
     if (!req.token) {
         return res.status(401).json({ error: 'token missing' });
     }
-    const decodedToken = jwt.verify(req.token, process.env.SECRET);
-    if (!decodedToken.id) {
-        return res.status(401).json({ error: 'token invalid' });
+
+    try {
+        const decodedToken = jwt.verify(req.token, process.env.SECRET);
+        if (!decodedToken.id) {
+            return res.status(401).json({ error: 'token invalid' });
+        }
+        req.user = await User.findById(decodedToken.id);
+        next();
+    } catch (error) {
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ error: 'token invalid or signature mismatch' });
+        } else if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: 'token expired' });
+        }
+        return res.status(401).json({ error: 'token verification failed' });
     }
-    req.user = await User.findById(decodedToken.id);
-    next();
 };
 
 module.exports = userExtractor;
